cf9c63552c2b7ba12053a6966dc28dc7f931f47d
mari_s <ms.mari.suzuki@gmail.com>
2020-05-08 20:28:54 +0900

    in progress 182. JavaFX Hello World program using GridPane root and Label greeting with methods - added a label in the center - changed the label text and font

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,8 +22,14 @@ public class Main extends Application {
         root.setVgap(10);
         root.setHgap(10);
 
+        Label greeting = new Label("Welcome to JavaFX!");
+        greeting.setTextFill(Color.GREEN);
+        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+
+        root.getChildren().add(greeting);
+
         primaryStage.setTitle("Hello JavaFX!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 275));
         primaryStage.show();
     }
 
